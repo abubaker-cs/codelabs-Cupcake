ecc09d9f9fd86ce43510dd16cebec4c7d1cc1c8b
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-03-27 15:12:48 +0500

    Working...

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,42 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        /**
+         * Result: This will do the following: Show a title in the app bar based off of the destination's label,
+         * and display the Up button whenever you're not on a top-level destination.
+         */
+        // Retrieve NavController from the NavHostFragment
+        val navHostFragment = supportFragmentManager
+                .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        // Make a call to setupActionBarWithNavController(navController) passing in the instance of NavController.
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
+    }
+
+    /**
+     * Handle navigation when the user chooses Up from the action bar.
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
